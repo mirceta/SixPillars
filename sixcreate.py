7e9eefb319dd0453cf411befda4fb689fad69006
kristijan.mirceta <kristijan.mirceta@gmail.com>
2018-10-28 15:43:01 +0100

    V1

---
+++ sixcreate.py
@@ -0,0 +1,94 @@
+####
+# Started from some folder $F, it will check in the resources subfolder to find
+# 1.txt, 2.txt, ... 6.txt, concatenate the files and write them to
+# a file named yyyy-MM-dd.txt for the current datetime.
+
+import os.path
+from os import listdir
+from os.path import isfile, join
+import datetime
+import argparse
+import sys
+
+
+directory = 'resources'
+headernames = ['THE PRACTICE OF ' + x for x in
+               ['LIVING CONSCIOUSLY', 'SELF-ACCEPTANCE', 'SELF-RESPONSIBILITY',
+                'SELF-ASSERTIVENESS', 'LIVING PURPOSEFULLY', 'PERSONAL INTEGRITY']]
+
+# helper methods
+def parseFileNameToObject(name):
+    parts = name.split('_')
+    habit = int(parts[0][0])
+    week = ''
+    timeofday = 1
+
+    for i in range(1, len(parts)):
+        if (parts[i].find("morning") != -1):
+            timeofday = 1
+            continue
+        if (parts[i].find("evening") != -1):
+            timeofday = 0
+            continue
+        if (parts[i].find("week") != -1):
+            week = parts[i][4:]
+            continue
+
+    return (habit, week, timeofday)
+
+# inside resources the files should be named 1.txt, 2.txt, ..., 6.txt for each
+# respective habit.
+
+#########################################################################
+## CODE
+
+print(sys.argv[1:])
+
+# parse arguments
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('habits', metavar='habits', type=int, nargs='+',
+                    help='the desired practices you want to include')
+parser.add_argument('--week', help='e.g. 1st week you are doing, put in 1. 2nd week, put in 2...')
+parser.add_argument('--morning', help="if it's morning put in 1, else 0.")
+
+args = parser.parse_args()
+
+
+if os.path.isdir(directory):
+
+    # get all files
+    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+    onlyfiles.sort()
+
+    ### filter only those that we want
+    A = [(parseFileNameToObject(f), directory + '/' + f) for f in onlyfiles]
+    # filter by habits
+    A = [x for x in A if x[0][0] in args.habits]
+    # filter by week
+    B = []
+    for x in A:
+        if x[0][1].find('+') != -1:
+            if int(x[0][1][0]) <= int(args.week):
+                B.append(x)
+        else:
+            if x[0][1] == args.week:
+                B.append(x)
+            if x[0][1] == '':
+                B.append(x)
+    A = B
+    # filter by part of day
+    A = [x for x in A if x[0][2] == int(args.morning)]
+
+    # write to file
+    allcontent = ''
+
+    for i in range(len(A)):
+
+        content = open(A[i][1], 'r').read()
+        allcontent += headernames[A[i][0][0] - 1] + '\n\n'
+        allcontent += content
+        allcontent += '\n\n'
+
+    resultfilename = str(datetime.datetime.now().strftime('%Y-%m-%d')) + '.md'
+
+    open(resultfilename, 'w').write(allcontent)
---
+++ sixgather.py
@@ -0,0 +1,99 @@
+import os.path
+from os import listdir
+from os.path import isfile, join
+from datetime import datetime, timedelta
+from collections import defaultdict
+
+# variables
+directory = 'resources'
+
+# methods
+def parseFileNameToObject(name):
+    parts = name.split('_')
+    habit = int(parts[0][0])
+    week = ''
+    timeofday = 1
+
+    for i in range(1, len(parts)):
+        if (parts[i].find("morning") != -1):
+            timeofday = 1
+            continue
+        if (parts[i].find("evening") != -1):
+            timeofday = 0
+            continue
+        if (parts[i].find("week") != -1):
+            week = parts[i][4:]
+            continue
+
+    return (habit, week, timeofday)
+
+#############################################
+## CODE
+
+# get current datetime
+startdate = datetime.now().strftime('%Y-%m-%d')
+currentdate = datetime.now()
+
+
+# get six pillars journal files contents for last 7 days
+query = []
+for i in range(1,8):
+    currentdate = currentdate - timedelta(days=1)
+    query.append(currentdate.strftime('%Y-%m-%d') + '.md')
+
+journalfilenames = [f for f in listdir('.') if isfile(f) and f.find('.md') != -1]
+journals = []
+for f in journalfilenames:
+    journals.append(open(f, 'r').readlines())
+
+
+# get all resource file contents, questions. Accumulate in the form hash[PRACTICE][QUESTION] = list(answers)
+resourcefilenames = [f for f in listdir(directory) if isfile(join(directory, f))]
+resourcefilenames.sort()
+
+headernames = ['THE PRACTICE OF ' + x for x in
+               ['LIVING CONSCIOUSLY', 'SELF-ACCEPTANCE', 'SELF-RESPONSIBILITY',
+                'SELF-ASSERTIVENESS', 'LIVING PURPOSEFULLY', 'PERSONAL INTEGRITY']]
+
+
+# put all those that have the same practice into the same array
+resourcefilecontentlist = [(parseFileNameToObject(f)[0], [line.rstrip() for line in open(directory + "/" + f, 'r').readlines()]) for f in resourcefilenames]
+practice_to_content_map = defaultdict(list)
+for o in resourcefilecontentlist:
+    practice_to_content_map[o[0]].extend(o[1])
+keys = [k for k in practice_to_content_map.keys()]
+keys.sort()
+resourcefilecontentlist = [practice_to_content_map[k] for k in keys]
+
+
+
+
+accumulator = {k: {l: list() for l in v} for k, v in zip(headernames, resourcefilecontentlist)}
+
+## fill up the accumulator
+journals = [[k.rstrip() for k in j] for j in journals]
+for journal in journals:
+    currentheadername = ''
+    currentquestion = ''
+    for line in journal:
+        if line in ['', ' ', '\n']:
+            continue
+        if line in headernames:
+            currentheadername = line
+            continue
+        if line in accumulator[currentheadername]:
+            currentquestion = line
+            continue
+        accumulator[currentheadername][currentquestion].append(line)
+
+## aggregate the results into a string
+resultfilecontent = ''
+for header in accumulator.keys():
+    resultfilecontent += "## " + header + "\n\n"
+    for question in accumulator[header].keys():
+        if len(accumulator[header][question]) > 0:
+            resultfilecontent += "### " + question + "\n\n"
+            resultfilecontent += '\n'.join(accumulator[header][question])
+            resultfilecontent += '\n\n'
+
+open('results/endofweek_' + startdate + '.md', 'w').write(resultfilecontent)
